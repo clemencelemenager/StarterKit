8a7517348ec7151aca4f42ab1262754a4ae32800
clemencelemenager <clemence.lemenager@gmail.com>
2020-11-08 18:28:41 +0100

    layout store page

--- app/Views/nav.tpl.php
+++ app/Views/nav.tpl.php
@@ -55,53 +55,52 @@
 <?php
 if ($currentURL === '/store') : ?>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light ">
+  <nav class="navbar navbar-expand-md navbar-light bg-light ">
 
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    Categories</span>
-  </button>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      Mon Shopping</span>
+    </button>
 
-  <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
 
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Item 1</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Item 2</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Item 3
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Item 4</a>
-      </li>
-    </ul>
+    <!-- List of categories -->
+      <ul class="navbar-nav">
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Voir tous les produits
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Category 1</a>
+            <a class="dropdown-item" href="#">Category 2</a>
+            <a class="dropdown-item" href="#">Category 3</a>
+            <a class="dropdown-item" href="#">Category 4</a>
+            <a class="dropdown-item" href="#">Category 5</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Sélection du mois</a>
+            <a class="dropdown-item" href="#">Déstockage</a>
+          </div>
+        </li>
+      </ul>
 
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+      <!-- Search form -->
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
 
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Account</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Panier</a>
-      </li>
-    </ul>
+      <!-- My account and Cart -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Mon compte</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Panier</a>
+        </li>
+      </ul>
 
-  </div>
-</nav>
+    </div>
+  </nav>
 
 <?php
 endif;
--- app/Views/store.tpl.php
+++ app/Views/store.tpl.php
@@ -1,7 +1,12 @@
-    <div class="container-fluid">
+    <div class="container-sm mx-sm-3">
 
-        <!-- carrousel -->
-            <div id="carouselExampleIndicators" class="carousel slide carousel-fade mb-3 mx-3" data-ride="carousel">
+
+        <!-- CARROUSEL -->
+
+        <div class="container-fluid">
+
+            <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel">
+
                 <ol class="carousel-indicators">
                     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
@@ -9,13 +14,13 @@
                 </ol>
                 <div class="carousel-inner">
                     <div class="carousel-item active" data-interval="200">
-                        <img src="<?= $baseUri ?>/images/plant_bg.jpeg" class="d-block w-100" alt="Lorem ipsum">
+                        <img src="<?= $assetsBaseUri ?>/images/plant_bg.jpeg" class="rounded d-block w-100" alt="Lorem ipsum">
                     </div>
                     <div class="carousel-item">
-                        <img src="<?= $baseUri ?>/images/green_blur.jpeg" class="d-block w-100" alt="...">
+                        <img src="<?= $assetsBaseUri ?>/images/green_blur.jpeg" class="rounded -block w-100" alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="<?= $baseUri ?>/images/plant_bg.jpeg" class="d-block w-100" alt="...">
+                        <img src="<?= $assetsBaseUri ?>/images/plant_bg.jpeg" class="rounded d-block w-100" alt="...">
                     </div>
                 </div>
                 <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -27,98 +32,190 @@
                     <span class="sr-only">Next</span>
                 </a>
             </div>
+
+        </div>
         
       
-        <!-- first row -->
+        <!-- CATEGORIES -->
 
-        <div class="row mx-0">
+        <div class="container-fluid">
 
-            <div class="col-md-6 mb-2">
-                <div class="card border-0 text-white text-center">
-                    <img src="<?= $baseUri ?>/images/plant_bg.jpeg" alt="" class="card-img">
-                    <div class="card-img-overlay d-flex align-items-center">
-                        <div class="w-100 py-3">
-                            <h2 class="display-3 font-weight-bold mb-4">
-                                Item 1
-                            </h2>
-                            <a href="#" class="btn btn-light">Découvrir</a>
+            <h2 class="my-4">Categories </h2>
+
+            <!-- first row -->
+
+            <div class="row">
+
+                <div class="col-lg-4 col-md-6 mb-2">
+                    <div class="card border-0 text-white text-center">
+                        <img src="<?= $assetsBaseUri ?>/images/plant_bg.jpeg" alt="" class="rounded card-img">
+                        <div class="card-img-overlay d-flex align-items-center">
+                            <div class="w-100 py-3">
+                                <h2 class="display-3 font-weight-bold mb-4">
+                                    Item 1
+                                </h2>
+                                <a href="#" class="btn btn-light">Découvrir</a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-6 mb-2">
-                <div class="card border-0 text-dark text-center">
-                    <img src="<?= $baseUri ?>/images/green_blur.jpeg" alt="" class="card-img">
-                    <div class="card-img-overlay d-flex align-items-center">
-                        <div class="w-100 py-3">
-                            <h2 class="display-3 font-weight-bold mb-4">
-                                Item 2
-                            </h2>
-                            <a href="#" class="btn btn-light">Découvrir</a>
+                <div class="col-lg-4 col-md-6 mb-2">
+                    <div class="card border-0 text-dark text-center">
+                        <img src="<?= $assetsBaseUri ?>/images/green_blur.jpeg" alt="" class="rounded card-img">
+                        <div class="card-img-overlay d-flex align-items-center">
+                            <div class="w-100 py-3">
+                                <h2 class="display-3 font-weight-bold mb-4">
+                                    Item 2
+                                </h2>
+                                <a href="#" class="btn btn-light">Découvrir</a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-
-        </div>
-
-        <!-- 2nd row -->
-
-        <div class="row mx-0">
-            
-            <div class="col-lg-4 col-md-6 mb-2">
-                <div class="card border-0 text-center text-dark">
-                    <img src="<?= $baseUri ?>/images/green_blur_2.jpeg"
-                    alt="Card image" class="card-img">
-                    <div class="card-img-overlay d-flex align-items-center">
-                    <div class="w-100">
-                        <h2 class="display-4 mb-4">
-                            Item 3
-                        </h2>
-                        <a href="category.html" class="btn btn-link text-dark">
-                            Lorem ipsum
-                        <i class="fa-arrow-right fa ml-2"></i></a>
+                <div class="col-lg-4 col-md-6 mb-2">
+                    <div class="card border-0 text-dark text-center">
+                        <img src="<?= $assetsBaseUri ?>/images/green_blur.jpeg" alt="" class="rounded card-img">
+                        <div class="card-img-overlay d-flex align-items-center">
+                            <div class="w-100 py-3">
+                                <h2 class="display-3 font-weight-bold mb-4">
+                                    Item 3
+                                </h2>
+                                <a href="#" class="btn btn-light">Découvrir</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
+
             </div>
-            </div>
-            <div class="col-lg-4 col-md-6 mb-2">
-                <div class="card border-0 text-center text-dark">
-                    <img src="<?= $baseUri ?>/images/green_blur_3.jpeg"
-                        alt="Card image" class="card-img">
-                    <div class="card-img-overlay d-flex align-items-center">
+
+            <!-- 2nd row -->
+
+            <div class="row">
+                
+                <div class="col-lg-3 col-md-6 mb-2">
+                    <div class="card border-0 text-center text-dark">
+                        <img src="<?= $assetsBaseUri ?>/images/green_blur_2.jpeg"
+                        alt="Card image" class="rounded card-img">
+                        <div class="card-img-overlay d-flex align-items-center">
                         <div class="w-100">
-                        <h2 class="display-4 mb-4">
-                            Item 4
-                        </h2>
-                        <a href="category.html" class="btn btn-link text-dark">
-                        Lorem ipsum
-                            <i class="fa-arrow-right fa ml-2"></i>
-                        </a>
+                            <h2 class="display-4 mb-4">
+                                Item 4
+                            </h2>
+                            <a href="category.html" class="btn btn-link text-dark">
+                                Lorem ipsum
+                            <i class="fa-arrow-right fa ml-2"></i></a>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-4 col-md-6 mb-2">
-                <div class="card border-0 text-center text-white">
-                    <img src="<?= $baseUri ?>/images/plant_bg.jpeg"
-                    alt="Card image" class="card-img">
-                    <div class="card-img-overlay d-flex align-items-center">
-                    <div class="w-100">
-                        <h2 class="display-4 mb-4">
-                            Item 5
-                        </h2>
-                        <a href="category.html" class="btn btn-link text-white">
-                        Lorem ipsum
-                        <i class="fa-arrow-right fa ml-2"></i></a>
-                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 mb-2">
+                    <div class="card border-0 text-center text-dark">
+                        <img src="<?= $assetsBaseUri ?>/images/green_blur_3.jpeg"
+                            alt="Card image" class="rounded card-img">
+                        <div class="card-img-overlay d-flex align-items-center">
+                            <div class="w-100">
+                            <h2 class="display-4 mb-4">
+                                Item 5
+                            </h2>
+                            <a href="category.html" class="btn btn-link text-dark">
+                            Lorem ipsum
+                                <i class="fa-arrow-right fa ml-2"></i>
+                            </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+                <div class="col-lg-3 col-md-6 mb-2">
+                    <div class="card border-0 text-center text-white">
+                        <img src="<?= $assetsBaseUri ?>/images/green_blur_2.jpeg"
+                        alt="Card image" class="rounded card-img">
+                        <div class="card-img-overlay d-flex align-items-center">
+                        <div class="w-100">
+                            <h2 class="display-4 mb-4">
+                                Item 6
+                            </h2>
+                            <a href="category.html" class="btn btn-link text-white">
+                            Lorem ipsum
+                            <i class="fa-arrow-right fa ml-2"></i></a>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 mb-2">
+                    <div class="card border-0 text-center text-white">
+                        <img src="<?= $assetsBaseUri ?>/images/green_blur_2.jpeg"
+                        alt="Card image" class="rounded card-img">
+                        <div class="card-img-overlay d-flex align-items-center">
+                        <div class="w-100">
+                            <h2 class="display-4 mb-4">
+                                Item 7
+                            </h2>
+                            <a href="category.html" class="btn btn-link text-white">
+                            Lorem ipsum
+                            <i class="fa-arrow-right fa ml-2"></i></a>
+                        </div>
+                        </div>
+                    </div>
+                </div>
             
+            </div>
         </div>
 
-        <div class="intro-text left-0 text-center p-3 rounded">
+        <!-- PRODUCTS -->
+
+        <div class="container-fluid mb-4">
+
+            <h2 class="my-4">Products </h2>
+
+            <div class="row">
+
+                <?php
+                for ($i=0; $i < 6 ; $i++) :?>
+
+                <div class="product col-xl-2 col-lg-4 col-sm-6 mb-5">
+                    <div class="product-image">
+                    <a href="#" class="product-hover-overlay-link">
+                        <img src="<?= $assetsBaseUri ?>/images/plant_bg.jpeg" alt="product" class="rounded img-fluid mb-2">
+                    </a>
+                    </div>
+                    <div class="py-2">
+                        <p class="text-muted text-sm mb-1">
+                            Type of product
+                        </p>
+                        <h3 class="h6 text-uppercase mb-1">
+                            <a href="#" class="text-dark">
+                                Name of product
+                            </a>
+                        </h3>
+                        <span class="text-muted">
+                            10.00 €
+                        </span>
+                    </div>
+                    <div class="product-action-buttons">
+                    <!-- TODO - add to cart -->
+                    <a href="#" class="btn btn-outline-dark btn-product-left"><i class="fa fa-shopping-cart"></i></a>
+                    <a href="#" class="btn btn-dark btn-buy">
+                        <i class="fa-search fa"></i>
+                        <span class="btn-buy-label ml-2">Voir</span>
+                    </a>
+                    </div>
+                </div>
+
+
+                <?php
+                endfor;
+                ?>
+                
+            </div>
+
+
+        </div>
+
+        <hr />
+
+        <!-- OPENING HOURS -->
+
+        <div class="container mt-4 intro-text left-0 text-center p-3 rounded">
             <h3 class="mb-4">Opening Hours</h3>
             <?php
             foreach ($viewVars["openingHours"] as $day => $hours) : ?>
