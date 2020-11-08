24ce5cd54424814df1291dba751e53756c2222ab
clemencelemenager <clemence.lemenager@gmail.com>
2020-11-08 15:00:07 +0100

    add carrousel /store

--- app/views/store.tpl.php
+++ app/views/store.tpl.php
@@ -1,5 +1,34 @@
 <section>
     <div class="container-fluid">
+
+        <!-- carrousel -->
+            <div id="carouselExampleIndicators" class="carousel slide carousel-fade mb-3 mx-3" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active" data-interval="200">
+                        <img src="<?= $viewVars["baseURL"] ?>/images/plant_bg.jpeg" class="d-block w-100" alt="Lorem ipsum">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="<?= $viewVars["baseURL"] ?>/images/green_blur.jpeg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="<?= $viewVars["baseURL"] ?>/images/plant_bg.jpeg" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        
       
         <!-- first row -->
 
